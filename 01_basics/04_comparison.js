321758302d22ffae0423f5b78558dc6445c85c02
YashBuilds <yash.singh.sstc.cseiot@gmail.com>
2025-06-10 18:54:53 +0000

    comparsion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 == 2);
+// console.log(2 != 2);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+
+
+// console.log("2" > 1);  // true (two different datatypes)
+// console.log("02" > 1); // true
+
+// console.log(null > 0);
+// console.log( null == 0);
+// console.log(null >= 0);
+
+
+// === Strict check 
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
